c882f11951d969ed41d7bbfe6cda40305019e044
liamdono <donovali@mail.uc.edu>
2023-06-11 02:44:24 -0400

    submit

--- src/SalesTaxCalculator.java
+++ src/SalesTaxCalculator.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class SalesTaxCalculator {
     public static void main(String[] args) {
-        // Create a Scanner object to read user input
+
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter the price of the purchase
+
         System.out.print("Enter the price of the purchase: ");
         double purchasePrice = scanner.nextDouble();
 
-        // Calculate the sales tax (5% of the purchase price)
+
         double salesTax = purchasePrice * 0.05;
 
-        // Compute the total price including sales tax
+
         double totalPrice = purchasePrice + salesTax;
 
-        // Output the sales tax and the total price
+
         System.out.println("Sales tax: $" + salesTax);
         System.out.println("Total price (including tax): $" + totalPrice);
 
